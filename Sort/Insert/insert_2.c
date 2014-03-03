e02a0d0921ecf546f2912d435465e4f2c31290ad
newmlee <matthewlee0725@gmail.com>
2014-03-03 18:22:15 +0000

    更新

--- Sort/Insert/insert_2.c
+++ Sort/Insert/insert_2.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/* 这是《算法导论》上伪代码的实现，
+ * 简化了insert_1.c中的算法，
+ * 主要将判断和移动合在了一起，
+ * 省去了原先的遍历过程
+ */
 int main()
 {
     int a[6] = {1, 3, 5, 4, 6, 2};
@@ -10,6 +15,7 @@ int main()
     for (j = 1; j < 6; j++) {
         temp = a[j];
         i = j - 1;
+        /* 关键在while()这一步 */
         while (i >= 0 && a[i] > temp) {
             a[i + 1] = a[i];
             i = i - 1;
