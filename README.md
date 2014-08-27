4d1bc239b7971857d2021e8309a1daef5bfc90f7
newmlee <matthewlee0725@gmail.com>
2014-08-27 09:44:36 +0000

    Reverse Words in a String

---
+++ README.md
@@ -0,0 +1,13 @@
+## LeetCode刷题集
+
+### 已完成：
+
+#### [Reverse Words in a String](https://oj.leetcode.com/submissions/detail/10373404/ "Reverse Words in a String")
+
+22 / 22 test cases passed.
+
+Runtime: 656 ms
+
+---
+
+未完待续。
---
+++ ReverseWordsInAString.java
@@ -0,0 +1,27 @@
+public class ReverseWordsInAString {
+    public static void main(String[] args) {
+        /* Do something with System.out.println() */
+    }
+}
+
+class Solution {
+    public String reverseWords(String s) {
+        String[] arr = s.split(" ");
+
+        String str = null;
+        try {
+            str = arr[arr.length - 1].trim();
+        } catch (ArrayIndexOutOfBoundsException a) {
+            return "";
+        } catch (NullPointerException n) {
+            return "";
+        }
+
+        for (int i = arr.length - 2; i > -1; i--) {
+            str = str.trim() + " " + arr[i].trim();
+        }
+
+        return str.trim();
+    }
+}
+
