b5c440fe0799bc2ee1c38d95439e95b9174e9a22
newmlee <matthewlee0725@gmail.com>
2014-08-27 10:02:58 +0000

    更新

--- README.md
+++ README.md
@@ -1,13 +1,7 @@
-## LeetCode刷题集
+这是我在[LeetCode OJ](https://oj.leetcode.com/ "LeetCode OJ")的刷题集，关键的思路都写在注释中。
 
-### 已完成：
+已完成：
 
-#### [Reverse Words in a String](https://oj.leetcode.com/submissions/detail/10373404/ "Reverse Words in a String")
+ - [Reverse Words in a String](https://oj.leetcode.com/submissions/detail/10373404/ "Reverse Words in a String")
 
-22 / 22 test cases passed.
-
-Runtime: 656 ms
-
----
-
-未完待续。
+ - 未完待续。
--- ReverseWordsInAString.java
+++ ReverseWordsInAString.java
@@ -1,13 +1,35 @@
+/*
+ * 22 / 22 test cases passed.
+ * Runtime: 656 ms
+ *
+ * 2014.08.27
+ */
+
 public class ReverseWordsInAString {
     public static void main(String[] args) {
-        /* Do something with System.out.println() */
+        /* System.out.println() */
     }
 }
 
 class Solution {
     public String reverseWords(String s) {
+        /* 调用String的split()方法进行分词 */
         String[] arr = s.split(" ");
 
+        /*
+         * 对于前导空格和后导空格，
+         * 可以使用String的trim()方法进行去除；
+         *
+         * 另外值得注意的是，
+         * split()方法在分割字符串的时候，
+         * 遇到第一个符合分割要求的字符就开始分配了，
+         * 所以当存在多个连续空格的时候，
+         * 使用split(" ")这样的方法并不能保证分割出来的字符串不包含空格；
+         * 如果要保证分割出来的字符串完全不包含空格，
+         * 一种方法是在split()方法中使用正则表达式，
+         * 二就是是连续不断地使用String的trim()方法，
+         * 当然啦，也不排除其他算法。
+         */
         String str = null;
         try {
             str = arr[arr.length - 1].trim();
