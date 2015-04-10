79df582dd2b2b4eaee923aba2cca3c9c55e4b3d4
newmlee <matthewlee0725@gmail.com>
2015-04-10 15:35:57 +0000

    20. Valid Parentheses

--- README.md
+++ README.md
@@ -42,6 +42,8 @@ __未完待续！！！__
 
  - [27. Remove Element](https://oj.leetcode.com/problems/remove-element/ "27. Remove Element")
 
+ - [20. Valid Parentheses](https://leetcode.com/problems/valid-parentheses/ "20. Valid Parentheses")
+
  - [14. Longest Common Prefix](https://oj.leetcode.com/problems/longest-common-prefix/ "14. Longest Common Prefix")
 
  - [9. Palindrome Number](https://oj.leetcode.com/problems/palindrome-number/ "9. Palindrome Number")
---
+++ ValidParentheses.java
@@ -0,0 +1,58 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        // TODO
+    }
+}
+
+class Solution {
+    private static final char ROUND_LEFT = '(';
+    private static final char ROUND_RIGHT = ')';
+
+    private static final char SQUARE_LEFT = '[';
+    private static final char SQUARE_RIGHT = ']';
+
+    private static final char BRACE_LEFT = '{';
+    private static final char BRACE_RIGHT = '}';
+
+    public boolean isValid(String string) {
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            switch (c) {
+                case ROUND_LEFT:
+                case SQUARE_LEFT:
+                case BRACE_LEFT:
+                    stack.push(c);
+                    break;
+                case ROUND_RIGHT:
+                    if (stack.empty() || stack.peek() != ROUND_LEFT) {
+                        return false;
+                    }
+                    stack.pop();
+                    break;
+                case SQUARE_RIGHT:
+                    if (stack.empty() || stack.peek() != SQUARE_LEFT) {
+                        return false;
+                    }
+                    stack.pop();
+                    break;
+                case BRACE_RIGHT:
+                    if (stack.empty() || stack.peek() != BRACE_LEFT) {
+                        return false;
+                    }
+                    stack.pop();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (!stack.empty()) {
+            return false;
+        }
+        return true;
+    }
+}
