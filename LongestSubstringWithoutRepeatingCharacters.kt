ffc92193a4138c7eef442179b3482a3fc70a8c17
Matthew Lee <matthewlee0725@gmail.com>
2018-12-30 16:15:56 +0800

    3.LongestSubstringWithoutRepeatingCharacters.java -> LongestSubstringWithoutRepeatingCharacters.kt

---
+++ LongestSubstringWithoutRepeatingCharacters.kt
@@ -0,0 +1,85 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Matthew Lee
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import java.util.*
+
+/*
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ *
+ * Given a string, find the length of the longest substring without repeating characters.
+ *
+ * Example 1:
+ * Input: "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ *
+ * Example 2:
+ * Input: "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ *
+ * Example 3:
+ * Input: "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3.
+ *              Note that the answer must be a substring,
+ *              "pwke" is a subsequence and not a substring.
+ */
+class LongestSubstringWithoutRepeatingCharacters {
+    fun lengthOfLongestSubstring(s: String): Int {
+        if (s.isEmpty()) {
+            return 0
+        }
+
+        var list = mutableListOf<Char>()
+        val set = mutableSetOf<Char>()
+        var maxLength = 0
+
+        for (c in s.toCharArray()) {
+            if (set.contains(c)) {
+                for (i in list.indices) {
+                    val e = list[i]
+                    set.remove(e)
+
+                    if (e == c) {
+                        if (i + 1 < list.size) {
+                            list = ArrayList(list.subList(i + 1, list.size))
+                        } else {
+                            list.clear()
+                        }
+                        break
+                    }
+                }
+            }
+
+            list.add(c)
+            set.add(c)
+            if (maxLength < list.size) {
+                maxLength = list.size
+            }
+        }
+
+        return maxLength
+    }
+}
