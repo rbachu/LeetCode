aa5b1bf60ea61dab1f89e3194b0c3e0ac2bdd291
Matthew Lee <matthewlee0725@gmail.com>
2018-12-30 16:58:27 +0800

    9.PalindromeNumber.java -> PalindromeNumber.kt

--- PalindromeNumber.kt
+++ PalindromeNumber.kt
@@ -22,20 +22,41 @@
  * THE SOFTWARE.
  */
 
-public final class Solution {
-    public boolean isPalindrome(int x) {
-        int m = x;
-        int n = 0;
+/*
+ * https://leetcode.com/problems/palindrome-number/
+ *
+ * Determine whether an integer is a palindrome.
+ * An integer is a palindrome when it reads the same backward as forward.
+ *
+ * Example 1:
+ * Input: 121
+ * Output: true
+ *
+ * Example 2:
+ * Input: -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ *
+ * Example 3:
+ * Input: 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ */
+class PalindromeNumber {
+    fun isPalindrome(input: Int): Boolean {
+        var x = input
+        val m = x
+        var n = 0
 
         if (x < 0) {
-            return false;
+            return false
         }
 
         while (x != 0) {
-            n = n * 10 + x % 10;
-            x = x / 10;
+            n = n * 10 + x % 10
+            x /= 10
         }
 
-        return (m == n);
+        return m == n
     }
 }
