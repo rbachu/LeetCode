a118c90fe2f4e29544a4865f468ba5bdca87ec99
Matthew Lee <matthewlee0725@gmail.com>
2018-12-30 16:32:46 +0800

    7.ReverseInteger.java -> ReverseInteger.kt

--- ReverseInteger.kt
+++ ReverseInteger.kt
@@ -22,30 +22,49 @@
  * THE SOFTWARE.
  */
 
-public final class Solution {
-    public int reverse(int x) {
-        int result = 0;
-        boolean isNeg = false;
-
-        if (x < 0) {
-            x = -x;
-            isNeg = true;
+/*
+ * https://leetcode.com/problems/reverse-integer/
+ *
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ *
+ * Example 1:
+ * Input: 123
+ * Output: 321
+ *
+ * Example 2:
+ * Input: -123
+ * Output: -321
+ *
+ * Example 3:
+ * Input: 120
+ * Output: 21
+ *
+ * Note:
+ * Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1].
+ * For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+ */
+class ReverseInteger {
+    fun reverse(input: Int): Int {
+        var x = Math.abs(input.toLong())
+        if (x > Int.MAX_VALUE) {
+            return 0
         }
-        
-        while (x / 10 != 0) {
-            result = result * 10 + x % 10;
-            x = x / 10;
-        }
-        result = result * 10 + x;
 
-        if (result > Integer.MAX_VALUE) {
-            return 0;
-        } else {
+        var result = 0L
+        val isNeg = input < 0
+
+        while (x / 10L != 0L) {
+            result = result * 10L + x % 10L
+            x /= 10L
+        }
+
+        result = result * 10L + x
+        return if (result > Integer.MAX_VALUE) { 0 } else {
             if (isNeg) {
-                result = -result;
+                result = -result
             }
-        }
 
-        return result;
+            result.toInt()
+        }
     }
 }
