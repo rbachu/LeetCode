aaac5b6df6415dc2f452f43ba319a48c8b4b8dcb
Matthew Lee <matthewlee0725@gmail.com>
2018-12-31 22:26:18 +0800

    LongestPalindromicSubstring.kt

---
+++ LongestPalindromicSubstring.kt
@@ -0,0 +1,77 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Matthew Lee
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/*
+ * https://leetcode.com/problems/longest-palindromic-substring/
+ *
+ * Given a string s, find the longest palindromic substring in s.
+ * You may assume that the maximum length of s is 1000.
+ *
+ * Example 1:
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ *
+ * Example 2:
+ * Input: "cbbd"
+ * Output: "bb"
+ */
+class LongestPalindromicSubstring {
+    fun longestPalindrome(s: String): String {
+        if (s.isEmpty()) {
+            return ""
+        }
+
+        var start = 0
+        var window = s.length
+        while (!isPalindromicString(s, start, start + window)) {
+            if (start + window == s.length) {
+                start = 0
+                window--
+            } else {
+                start++
+            }
+
+            if (start > s.lastIndex || window < 0) {
+                return ""
+            }
+        }
+
+        return s.substring(start, start + window)
+    }
+
+    private fun isPalindromicString(s: String, from: Int, to: Int): Boolean {
+        var start = from
+        var end = to - 1
+        while (s[start] == s[end]) {
+            start++
+            end--
+            if (start > s.lastIndex || end < 0) {
+                break
+            }
+        }
+
+        return start >= end
+    }
+}
