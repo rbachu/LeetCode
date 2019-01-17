6d3b6b93a8aa64b8e193c62fd8209939740424c5
Matthew Lee <matthewlee0725@gmail.com>
2019-01-17 18:08:25 +0800

    ContainerWithMostWater.kt

---
+++ ContainerWithMostWater.kt
@@ -0,0 +1,62 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Matthew Lee
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/*
+ * https://leetcode.com/problems/container-with-most-water/
+ *
+ * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
+ * n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+ * Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+ *
+ * Note: You may not slant the container and n is at least 2.
+ *
+ * Example:
+ * Input: [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ */
+class ContainerWithMostWater {
+    fun maxArea(height: IntArray): Int {
+        var leftIndex = 0
+        var rightIndex = height.lastIndex
+        var maxArea = 0
+
+        while (leftIndex < rightIndex) {
+            val leftHeight = height[leftIndex]
+            val rightHeight = height[rightIndex]
+
+            val area = Math.abs(leftIndex - rightIndex) * Math.min(leftHeight, rightHeight)
+            if (maxArea < area) {
+                maxArea = area
+            }
+
+            if (leftHeight < rightHeight) {
+                leftIndex++
+            } else {
+                rightIndex--
+            }
+        }
+
+        return maxArea
+    }
+}
