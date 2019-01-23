0ec85b6a5b532100aba449dce36982f75eaec3c3
Matthew Lee <matthewlee0725@gmail.com>
2019-01-23 17:43:09 +0800

    ThreeSumClosest.kt

---
+++ ThreeSumClosest.kt
@@ -0,0 +1,59 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Matthew Lee
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/*
+ * https://leetcode.com/problems/3sum-closest/
+ *
+ * Given an array nums of n integers and an integer target,
+ * find three integers in nums such that the sum is closest to target.
+ * Return the sum of the three integers.
+ * You may assume that each input would have exactly one solution.
+ *
+ * Example:
+ * Given array nums = [-1, 2, 1, -4], and target = 1.
+ * The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ */
+class ThreeSumClosest {
+    fun threeSumClosest(nums: IntArray, target: Int): Int {
+        nums.sort()
+
+        var closest = nums[nums.lastIndex] + nums[nums.lastIndex - 1] + nums[nums.lastIndex - 2]
+
+        for (i in 0 until nums.size - 2) {
+            var start = i + 1
+            var end = nums.lastIndex
+
+            while (start < end) {
+                val sum = nums[i] + nums[start] + nums[end]
+                when {
+                    Math.abs(sum - target) < Math.abs(closest - target) -> closest = sum
+                    sum < target -> start++
+                    else -> end--
+                }
+            }
+        }
+
+        return closest
+    }
+}
