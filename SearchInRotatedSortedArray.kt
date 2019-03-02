9b7b6c897b0c66523e6d98aa84fee968c97f5192
Matthew Lee <matthewlee0725@gmail.com>
2019-03-02 14:41:19 +0800

    SearchInRotatedSortedArray

---
+++ SearchInRotatedSortedArray.kt
@@ -0,0 +1,79 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Matthew Lee
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/*
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+ *
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ * You may assume no duplicate exists in the array.
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ *
+ * Example 1:
+ * Input: nums = [4,5,6,7,0,1,2], target = 0
+ * Output: 4
+ *
+ * Example 2:
+ * Input: nums = [4,5,6,7,0,1,2], target = 3
+ * Output: -1
+ */
+class SearchInRotatedSortedArray {
+    fun search(nums: IntArray, target: Int): Int {
+        if (nums.isEmpty()) {
+            return -1
+        }
+
+        var start = 0
+        var end = nums.lastIndex
+
+        while (start <= end) {
+            val index = start + (end - start) / 2
+            if (nums[index] == target) {
+                return index
+            }
+
+            if (nums[start] <= nums[index]) {
+                if (target < nums[index] && target >= nums[start]) {
+                    end = index - 1
+                } else {
+                    start = index + 1
+                }
+            } else {
+                if (target > nums[index] && target <= nums[end]) {
+                    start = index + 1
+                } else {
+                    end = index - 1
+                }
+            }
+        }
+
+        return -1
+    }
+}
+
+fun main() {
+    println(SearchInRotatedSortedArray().search(intArrayOf(7, 9, 1, 4, 6), 5))
+}
